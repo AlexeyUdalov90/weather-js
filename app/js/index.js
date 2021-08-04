60f20990396b6fb3f57e343530dba3fd1ae2d12a
Clay1612 <dedsec1612@gmail.com>
2021-08-04 09:52:41 +0300

    конфигурация вебпака

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,6 @@
-import "/app/scss/style.scss";
+import '/app/scss/style.scss';
+import '/app/assets/images/orig.jpg'
+import '/app/assets/images/youtube.svg'
 
 console.log('Hello world!')
 alert('How are you?');
