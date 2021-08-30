0afc1ff33f45b89017c5d1db3ea770a4d01ff994
Clay1612 <dedsec1612@gmail.com>
2021-08-30 08:33:10 +0300

    замена fetch на axios

--- app/js/api.js
+++ app/js/api.js
@@ -1,16 +1,16 @@
-'use strict'
+import axios from 'axios';
 
-fetch('http://api.openweathermap.org/data/2.5/weather?id=472757&appid=dcb827e237f5f1a3c296d1c3322ba770')
+axios
+  .get('http://api.openweathermap.org/data/2.5/weather' , {
+    params: {
+      id: '472757',
+      appid: 'dcb827e237f5f1a3c296d1c3322ba770',
+    },
+  })
   .then(
     function (response) {
-      return response.json()
-    }
-  )
-  .then(
-    function (data) {
-      console.log(data)
-      document.querySelector('.test__api-city').innerHTML = data.name;
-      document.querySelector('.test__api-degree').innerHTML = Math.round(data.main.temp - 273);
-      document.querySelector('.test__api-weather').innerHTML = data.weather[0].main;
+      document.querySelector('.test__api-city').innerHTML = response.data.name;
+      document.querySelector('.test__api-degree').innerHTML = Math.round(response.data.main.temp - 273);
+      document.querySelector('.test__api-weather').innerHTML = response.data.weather[0].main;
     }
   )
